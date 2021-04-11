7f27eaf1422e3210e4efb31aebd68e9e9c4845b8
yosightpro <gates22.bn@gmail.com>
2021-04-11 23:03:18 +0300

    First Scratch

---
+++ vu/src/app/client/about.php
@@ -0,0 +1,327 @@
+<?php include('header.php'); ?>
+    <!-- /breadcrumbs-->
+    <!--<div class="container" style="border: red 1px solid">-->
+        <nav aria-label="breadcrumb" class="breadcrumb-info">
+          <ol class="breadcrumb mb-0">
+            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">About Us</li>
+          </ol>
+        </nav>
+      <!--</div>-->
+          <!-- //breadcrumbs-->
+	</div>
+	<!--//banner-slider-->
+</section>
+
+<section class="w3l-apply-6">
+	<!-- /apply-6-->
+	<div class="apply-info py-5">
+		<div class="container py-lg-5">
+			<br>
+			<br>
+			<br>
+			<br>
+			<div class="">
+					<h4 class="text-center"><span style="border-left: 4px #D4484A solid">&nbsp;About VU</span></h4>
+				</div>
+			<div class="apply-grids-info row">
+					<div class="row mt-lg-5 mt-4">
+						<div class="appyl-sub-icon-info text-center">
+							<p>Victoria University is one of the leading universities in Uganda and stands out as a center for academic excellence. It offers a fresh and intellectually stimulating environment that nurtures critical thinkers.The University was opened in August 2013 and has the capacity, the facilities and determination to revitalize higher education in Uganda and in the region. We are committed to playing a leading role in bringing and developing high quality education, student centered learning opportunities based in standards of excellence that are unique, innovative and difficult to match.</p>
+						</div>
+					</div>
+				
+			</div>
+		</div>
+	</div>
+</section>
+<!--about banner-->
+<section class="w3l-wecome-content-6">
+	<!-- /content-6-section -->
+	  <div class="ab-content-6-mian py-5">
+			 <div class="container py-lg-5">
+					<div class="welcome-grids row">
+							<div class="welcome-image">
+								<img src="assets/images/about_strategy.png" class="img-fluid" alt="" />
+							</div>	
+						
+						</div>	
+				 
+				 </div>
+			 </div>
+	 </section>
+<section class="w3l-wecome-content-6">
+	<!-- /content-6-section -->
+	  <div class="ab-content-6-mian py-5">
+			 <div class="container py-lg-5">
+					<div class="welcome-grids row">
+							<div class="col-lg-6 mb-lg-0 mb-5">
+								<h6>About Us</h6>
+								<h3 class="hny-title">
+									Victoria University
+									</h3>
+								<p class="my-4">Victoria University is part of the Ruparelia Group of Companies, which has a strong presence in the education sector in Uganda and has under its portfolio, Kampala Parents School and Delhi Public School International.</p>
+								<p class="mb-4">Victoria University is a cosmopolitan university, accredited by the National Council for Higher Education with a wide range of nationalities. With a reputation as the best private, forward thinking and modern university in East Africa, Victoria University offers a vibrant and stimulating environment to further your studies. Our programmes are designed to meet all of your needs, study or research a subject in greater depth, or aiming to extend your knowledge and skills in pursuit of your career ambitions.</p>
+							</div>
+							<div class="col-lg-6 welcome-image" style="border: #000 1px solid">
+								<div class="apply-gd-right col-lg-5 mt-lg-0 mt-5">
+	
+					<div class="apply-form p-md-5 p-4 mx-auto bg-white mw-100">
+						<form action="#" method="post">
+							<div class="form-group">
+								<h2>Missions and staff</h2>
+								<label>First name</label>
+
+								<input type="text" class="form-control" id="validationDefault01" placeholder=""
+									required="">
+							</div>
+							<div class="form-group">
+								<label>Last name</label>
+								<input type="text" class="form-control" id="validationDefault02" placeholder=""
+									required="">
+							</div>
+
+							<div class="form-group mb-4">
+								<label class="mb-2">Password</label>
+								<input type="password" class="form-control" id="password1" placeholder="" required="">
+							</div>
+
+							<button type="submit" class="btn btn-primary submit">Apply Now</button>
+
+						</form>
+
+					</div>
+				</div>
+							</div>	
+						
+						</div>	
+				 
+				 </div>
+			 </div>
+	 </section>
+   <!-- //content-6-section -->
+
+  
+<section class="w3l-specification-6">
+	<!-- /specification-6-->
+	<div class="specification-content py-5">
+		<div class="container py-lg-5">
+			<div class="mission-grids-info row">
+				<div class="mission-gd-left col-lg-4" style="border: #000 2px solid;">
+					<div class="grids-inn-ab">
+						<!---->
+						<div class="features-1-info col-lg-8 col-md-6">
+							<div class="features-1-info-info text-center">
+								<h3 class="text-center"><a href="#">Stats coming soon...</a></h3>
+								<p>Requirements</p>
+								<p>Date &amp; Deadlines</p>
+								<br>
+								<h3 class="text-center"><a href="#">Admissions</a></h3>
+								<p>Requirements</p>
+								<p>Date &amp; Deadlines</p>
+								<br>
+								<h3 class="text-center"><a href="#">Admissions</a></h3>
+								<p>Requirements</p>
+								<p>Date &amp; Deadlines</p>
+							</div>
+						</div>
+						<!---->
+					</div>
+				</div>
+				<div class="mission-gd-right col-lg-8 pl-lg-4">
+
+						<h3 class="hny-title">VU at a Glance</h3>
+						<p>Victoria University is one of the leading universities in Uganda and stands out as a center for academic excellence. It offers a fresh and intellectually stimulating environment that nurtures critical thinkers.The University was opened in August 2013 and has the capacity, the facilities and determination to revitalize higher education in Uganda and in the region. We are committed to playing a leading role in bringing and developing high quality education, student centered learning opportunities based in standards of excellence that are unique, innovative and difficult to match.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //specification-6-->
+
+<section class="w3l-content-5">
+	<!-- /content-6-section -->
+	  <div class="content-5-main">
+			 <div class="container">
+					 <div class="content-info-in row">
+							 <div class="content-gd col-lg-6">
+									 <h3 class="hny-title two">Objectives</h3>
+							 </div>
+							 <div class="content-gd col-lg-6">
+									<p>To provide transformational education by providing dynamic learning environment.</p>
+									 <p>To support creation of new knowledge by Research, Innovation and Critical Thinking.</p>
+								 	<p>To provide opportunities of community engagements.</p>
+							 </div>
+							
+					 </div>
+				 
+				 </div>
+			 </div>
+	 </section>
+   <!-- //content-6-section -->
+
+  
+	<!--/team-sec-->
+	<section class="w3l-team-main">
+		<div class="team py-5">
+			<div class="container py-lg-5">
+					<div class="row title-content">
+							<div class="col-lg-4 title-left">
+								<h3 class="hny-title">Expert Instructors</h3>
+							</div>
+							<div class="col-lg-8 title-info">
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi minima
+									accusantium
+									reiciendis, cupiditate optio corrupti quis quam at!.Duis aute irure dolor in reprehenderit
+									in voluptate velit esse cillum.</p>
+							</div>
+						</div>
+					<div class="row team-row">
+							<div class="col-lg-3 col-md-6 team-wrap">
+									<div class="team-info text-center">
+										<div class="column position-relative">
+											<a href="#url"><img src="assets/images/team1.jpg" alt="" class="img-fluid team-image" /></a>
+										</div>
+										<div class="column">
+											<h3 class="name-pos"><a href="#url">Mickel Zaman</a></h3>
+											<p>Law Instructor</p>
+											<div class="social">
+												<a href="#facebook" class="facebook"><span class="fa fa-facebook" aria-hidden="true"></span></a>
+												<a href="#twitter" class="twitter"><span class="fa fa-twitter" aria-hidden="true"></span></a>
+											</div>
+									</div>
+								</div>
+							</div>
+							<!-- end team member -->
+				
+							<div class="col-lg-3 col-md-6 team-wrap">
+			
+									<div class="team-info text-center">
+											<div class="column position-relative">
+												<a href="#url"><img src="assets/images/team2.jpg" alt="" class="img-fluid team-image" /></a>
+											</div>
+											<div class="column">
+												<h3 class="name-pos"><a href="#url">Paul Croves</a></h3>
+												<p>English Instructor</p>
+												<div class="social">
+													<a href="#facebook" class="facebook"><span class="fa fa-facebook" aria-hidden="true"></span></a>
+													<a href="#twitter" class="twitter"><span class="fa fa-twitter" aria-hidden="true"></span></a>
+												</div>
+										</div>
+									</div>
+							</div>
+							<!-- end team member -->
+				
+							<div class="col-lg-3 col-md-6 team-wrap">
+									<div class="team-info text-center">
+											<div class="column position-relative">
+												<a href="#url"><img src="assets/images/team3.jpg" alt="" class="img-fluid team-image" /></a>
+											</div>
+											<div class="column">
+												<h3 class="name-pos"><a href="#url">Ricardo Spencer</a></h3>
+												<p>Gym Instructor</p>
+												<div class="social">
+													<a href="#facebook" class="facebook"><span class="fa fa-facebook" aria-hidden="true"></span></a>
+													<a href="#twitter" class="twitter"><span class="fa fa-twitter" aria-hidden="true"></span></a>
+												</div>
+										</div>
+									</div>
+								
+							</div>
+							<!-- end team member -->
+							<!-- end team member -->
+				
+							<div class="col-lg-3 col-md-6 team-wrap">
+			
+									<div class="team-info text-center">
+											<div class="column position-relative">
+												<a href="#url"><img src="assets/images/team4.jpg" alt="" class="img-fluid team-image" /></a>
+											</div>
+											<div class="column">
+												<h3 class="name-pos"><a href="#url">Marko Dugonji</a></h3>
+												<p>Law Instructor</p>
+												<div class="social">
+													<a href="#facebook" class="facebook"><span class="fa fa-facebook" aria-hidden="true"></span></a>
+													<a href="#twitter" class="twitter"><span class="fa fa-twitter" aria-hidden="true"></span></a>
+												</div>
+										</div>
+									</div>
+							</div>
+							<!-- end team member -->
+							<div class="col-lg-3 col-md-6 team-wrap">
+									<div class="team-info text-center">
+										<div class="column position-relative">
+											<a href="#url"><img src="assets/images/team5.jpg" alt="" class="img-fluid team-image" /></a>
+										</div>
+										<div class="column">
+											<h3 class="name-pos"><a href="#url">Anthony</a></h3>
+											<p>Physics Instructor</p>
+											<div class="social">
+												<a href="#facebook" class="facebook"><span class="fa fa-facebook" aria-hidden="true"></span></a>
+												<a href="#twitter" class="twitter"><span class="fa fa-twitter" aria-hidden="true"></span></a>
+											</div>
+									</div>
+								</div>
+							</div>
+							<!-- end team member -->
+				
+							<div class="col-lg-3 col-md-6 team-wrap">
+			
+									<div class="team-info text-center">
+											<div class="column position-relative">
+												<a href="#url"><img src="assets/images/team6.jpg" alt="" class="img-fluid team-image" /></a>
+											</div>
+											<div class="column">
+												<h3 class="name-pos"><a href="#url">Emma Stone</a></h3>
+												<p>Maths Instructor</p>
+												<div class="social">
+													<a href="#facebook" class="facebook"><span class="fa fa-facebook" aria-hidden="true"></span></a>
+													<a href="#twitter" class="twitter"><span class="fa fa-twitter" aria-hidden="true"></span></a>
+												</div>
+										</div>
+									</div>
+							</div>
+							<!-- end team member -->
+				
+							<div class="col-lg-3 col-md-6 team-wrap">
+									<div class="team-info text-center">
+											<div class="column position-relative">
+												<a href="#url"><img src="assets/images/team7.jpg" alt="" class="img-fluid team-image" /></a>
+											</div>
+											<div class="column">
+												<h3 class="name-pos"><a href="#url">Rhoda Byrd</a></h3>
+												<p>Finance Instructor</p>
+												<div class="social">
+													<a href="#facebook" class="facebook"><span class="fa fa-facebook" aria-hidden="true"></span></a>
+													<a href="#twitter" class="twitter"><span class="fa fa-twitter" aria-hidden="true"></span></a>
+												</div>
+										</div>
+									</div>
+								
+							</div>
+							<!-- end team member -->
+							<!-- end team member -->
+				
+							<div class="col-lg-3 col-md-6 team-wrap">
+			
+									<div class="team-info text-center">
+											<div class="column position-relative">
+												<a href="#url"><img src="assets/images/team8.jpg" alt="" class="img-fluid team-image" /></a>
+											</div>
+											<div class="column">
+												<h3 class="name-pos"><a href="#url">Max Stoiber</a></h3>
+												<p>History Instructor</p>
+												<div class="social">
+													<a href="#facebook" class="facebook"><span class="fa fa-facebook" aria-hidden="true"></span></a>
+													<a href="#twitter" class="twitter"><span class="fa fa-twitter" aria-hidden="true"></span></a>
+												</div>
+										</div>
+									</div>
+							</div>
+							<!-- end team member -->
+				</div>
+			</div>
+	</section>
+	<!--//team-sec-->
+<!-- footer -->
+<?php include_once('footer.php'); ?>
