523560596b73f4570d61b9fbe5f0288857316109
yosightpro <gates22.bn@gmail.com>
2021-06-19 15:23:57 +0300

    Bread-crumb Fix with select path

--- src/client/src/client/components/trustees.js
+++ src/client/src/client/components/trustees.js
@@ -20,7 +20,7 @@ class Trustees extends Component {
             <React.Fragment>
                 <BreadNavBar />
                 <NavBar />
-<section class="w3l-top-header-content" style={{marginTop: '-25px'}}>
+<section class="w3l-top-header-content">
 	<div class="hny-top-menu">
 		<div class="top-hd" style={{padding: '5px 0px 5px 0px', backgroundColor:'#024c74'}}>
 			<div class="container-fluid">
