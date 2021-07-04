9ed61698876803ae0dca5eb3174e2ea6e8542aa8
Harold Wanyama <wanyaland+nickmango@gmail.com>
2021-07-05 02:10:03 +0300

    Continous deploy setup
    Signed-off-by: Harold Wanyama <wanyaland+nickmango@gmail.com>

--- src/deploy_project.sh
+++ src/deploy_project.sh
@@ -1,5 +1,11 @@
-cd /home/circleci/victoria_university
+#!/bin/sh
 
-git pull origin main
-
-docker-compose -f src/docker-compose.prod.yaml up -d --build
+ssh -o StrictHostKeyChecking=no -v $CIRCLECI_USER@$VU_IP << 'ENDSSH'
+  cd /home/circleci/victoria_university
+  export $(cat src/app/.env.dev | xargs)
+  docker login -u $DOCKER_USER -p $DOCKER_PASS
+  docker pull $IMAGE:web
+  docker pull $IMAGE:api
+  docker pull $IMAGE:nginx
+  docker-compose -f docker-compose.prod.yml up -d
+ENDSSH
