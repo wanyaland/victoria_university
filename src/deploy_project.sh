e51cf1dcdefd7fdd37013afc12ef113f3603c04f
Harold Wanyama <wanyaland+nickmango@gmail.com>
2021-07-05 11:30:01 +0300

    allowed host conf setup
    Signed-off-by: Harold Wanyama <wanyaland+nickmango@gmail.com>

--- src/deploy_project.sh
+++ src/deploy_project.sh
@@ -5,6 +5,5 @@ ssh -o StrictHostKeyChecking=no -v $CIRCLECI_USER@$VU_IP << 'ENDSSH'
   eval "$(ssh-agent -s)"
   echo "Getting GH updates" && git pull origin main
   export $(cat src/app/.env | xargs)
-  chmod +x src/entrpoint-prod.sh
   docker-compose -f src/docker-compose.prod.yaml up -d --build
 ENDSSH
