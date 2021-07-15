eb7c8536f433a5f3f964ca8e86b4d30c1fa9e12d
Harold Wanyama <wanyaland+nickmango@gmail.com>
2021-07-16 01:51:17 +0300

    CD update
    Signed-off-by: Harold Wanyama <wanyaland+nickmango@gmail.com>

--- src/deploy_project.sh
+++ src/deploy_project.sh
@@ -2,10 +2,8 @@
 
 ssh -o StrictHostKeyChecking=no -v $CIRCLECI_USER@$VU_IP << 'ENDSSH'
   cd /home/circleci/app
-  eval "$(ssh-agent -s)"
-  echo "Getting GH updates" && git pull origin main
   export $(cat .env | xargs)
   docker pull $IMAGE:web
   docker pull $IMAGE:api
-  docker-compose -f docker-compose.prod.yml up -d
+  docker-compose -f docker-compose.prod.yaml up -d
 ENDSSH
