355dec4b65d75942a2f78db8f2b890fb2d1dea29
Harold Wanyama <hwanyama@Harolds-MBP-2.local.tld>
2021-07-20 12:19:35 +0300

    update client image
    Signed-off-by: Harold Wanyama <wanyaland@gmail.com>

--- src/deploy_project.sh
+++ src/deploy_project.sh
@@ -4,7 +4,7 @@ ssh -o StrictHostKeyChecking=no -v $CIRCLECI_USER@$VU_IP << 'ENDSSH'
   cd /home/circleci/app
   export $(cat .env | xargs)
   docker login -u $DOCKER_USER -p $DOCKER_PASS
-  docker pull $IMAGE:web
+  docker pull $IMAGE:web_latest
   docker pull $IMAGE:api
   docker pull $IMAGE:nginx
   docker-compose -f docker-compose.prod.yaml up -d
