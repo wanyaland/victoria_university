56156e83c1917cce0df76c6e0b63886fd3c31198
Harold Wanyama <hwanyama@Harolds-MBP-2.local.tld>
2021-07-21 22:45:27 +0300

    apyly named images
    Signed-off-by: Harold Wanyama <hwanyama@Harolds-MBP-2.local.tld>

--- src/deploy_project.sh
+++ src/deploy_project.sh
@@ -4,8 +4,8 @@ ssh -o StrictHostKeyChecking=no -v $CIRCLECI_USER@$VU_IP << 'ENDSSH'
   cd /home/circleci/app
   export $(cat .env | xargs)
   docker login -u $DOCKER_USER -p $DOCKER_PASS
-  docker pull web
-  docker pull api
-  docker pull nginx
+  docker pull wanyaland/vu_web
+  docker pull wanyaland/vu_api
+  docker pull wanyaland/vu_nginx
   docker-compose -f docker-compose.prod.yaml up -d
 ENDSSH
