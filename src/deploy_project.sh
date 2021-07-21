cce56835c94b08ba52d79650e147faf2f914e5f4
Harold Wanyama <hwanyama@Harolds-MBP-2.local.tld>
2021-07-21 23:21:13 +0300

    latest image
    Signed-off-by: Harold Wanyama <hwanyama@Harolds-MBP-2.local.tld>

--- src/deploy_project.sh
+++ src/deploy_project.sh
@@ -4,8 +4,8 @@ ssh -o StrictHostKeyChecking=no -v $CIRCLECI_USER@$VU_IP << 'ENDSSH'
   cd /home/circleci/app
   export $(cat .env | xargs)
   docker login -u $DOCKER_USER -p $DOCKER_PASS
-  docker pull wanyaland/vu_web
-  docker pull wanyaland/vu_api
-  docker pull wanyaland/vu_nginx
+  docker pull wanyaland/vu_web:latest
+  docker pull wanyaland/vu_api:latest
+  docker pull wanyaland/vu_nginx:latest
   docker-compose -f docker-compose.prod.yaml up -d
 ENDSSH
