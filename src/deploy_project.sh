94b10044cc569fdbcb6520226d81f3f8393ee413
Harold Wanyama <hwanyama@Harolds-MBP-2.local.tld>
2021-07-21 22:01:51 +0300

    use latest tags on docker images
    Signed-off-by: Harold Wanyama <hwanyama@Harolds-MBP-2.local.tld>

--- src/deploy_project.sh
+++ src/deploy_project.sh
@@ -4,8 +4,8 @@ ssh -o StrictHostKeyChecking=no -v $CIRCLECI_USER@$VU_IP << 'ENDSSH'
   cd /home/circleci/app
   export $(cat .env | xargs)
   docker login -u $DOCKER_USER -p $DOCKER_PASS
-  docker pull $IMAGE:web
-  docker pull $IMAGE:api
-  docker pull $IMAGE:nginx
+  docker pull web
+  docker pull api
+  docker pull nginx
   docker-compose -f docker-compose.prod.yaml up -d
 ENDSSH
