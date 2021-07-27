b8766a9fb20f38fa1efa29c1ffa2e02e0aad3163
Harold Wanyama <wanyaland@gmail.com>
2021-07-28 01:37:32 +0300

    Feature/SSL Config
    - Leveraged LetsEncrypt for nginx setup
    
    Signed-off-by: Harold Wanyama <wanyaland@gmail.com>

---
+++ src/deploy_staging.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ssh -o StrictHostKeyChecking=no -v $CIRCLECI_USER@$VU_IP << 'ENDSSH'
+  cd /home/circleci/app
+  export $(cat .env | xargs)
+  export $(cat .env.staging.proxy-companion | xargs)
+  docker login -u $DOCKER_USER -p $DOCKER_PASS
+  docker pull wanyaland/vu_web:latest
+  docker pull wanyaland/vu_api:latest
+  docker pull wanyaland/vu_nginx:latest
+  docker-compose -f docker-compose.prod.yaml up -d
+ENDSSH
