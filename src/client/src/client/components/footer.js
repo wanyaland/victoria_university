e87cb926c9704d01288b77cd802a1b27bb68143e
Harold Wanyama <hwanyama@seismic.com>
2021-10-10 16:19:32 +0300

    Bug/Rendering
    - Resolved render for 16 inch machine
    
    Signed-off-by: Harold Wanyama <hwanyama@seismic.com>

--- src/client/src/client/components/footer.js
+++ src/client/src/client/components/footer.js
@@ -31,7 +31,6 @@ class Footer extends Component{
             <React.Fragment>
             <Chat />
             <PayPop />
-            <span id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=elFld1AV4L5eoI9oxn1qLKb1xoAAmYvZvWDiT81XlryapnR1ZPqwOuWdpKsH"></script></span>
         <section className="w3l-footer-16">
     <div className="w3l-footer-16-main py-5">
         <div className="container">
@@ -47,8 +46,9 @@ class Footer extends Component{
                     </div>
                 </div>
             </div>
+                    <span id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=elFld1AV4L5eoI9oxn1qLKb1xoAAmYvZvWDiT81XlryapnR1ZPqwOuWdpKsH"></script></span>
 					<div className="appyl-sub-icon-info">
-						<p style={{color: '#FFF'}}>Victoria University is home to opportunities. It’s where you’ll leave behind what’s familiar and step into a world of endless possibilities. Who you are, what you want, where you’ll go, and how you can impact your community, country and the world are yours to discover.</p>
+						<p style={{color: '#FFF'}}>Victoriadsadsa University is home to opportunities. It’s where you’ll leave behind what’s familiar and step into a world of endless possibilities. Who you are, what you want, where you’ll go, and how you can impact your community, country and the world are yours to discover.</p>
 					</div>
 					<div className="columns-2 mt-md-0 mt-3">
 						<ul className="social text-right">
