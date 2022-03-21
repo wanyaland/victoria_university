76cedf7b7cf72096de309a6ecb96e3d9408980a4
Kana-creator <82591192+Kana-creator@users.noreply.github.com>
2022-03-21 05:27:19 -0700

    Update policy-list.js

--- src/client/src/client/components/policy-list.js
+++ src/client/src/client/components/policy-list.js
@@ -4,7 +4,7 @@ const PolicyList = (props) => {
     return(
         <React.Fragment>
             {props.policylist.map((item) => (
-//                 <li className="underline lister" style={{}}key={item.id}><a href={`assets/policies/${item.name}`} download={`assets/policies/${item.name}`}>{item.name} <span className="fa fa-file-pdf-o" style={{color: '#f74040'}}></span></a></li>
+                <li className="underline lister" style={{}}key={item.id}><a href={`assets/policies/${item.name}`} download={`assets/policies/${item.name}`}>{item.name} <span className="fa fa-file-pdf-o" style={{color: '#f74040'}}></span></a></li>
             ))}
         </React.Fragment>
     )
