0c79d0bfeb999a0f0303e8de47ad11098ea31c29
Kana-creator <akuwebwa@gmail.com>
2022-03-23 10:57:48 -0700

    Edits made

--- src/client/src/client/components/policy-list.js
+++ src/client/src/client/components/policy-list.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const PolicyList = (props) => {
-    return(
-        <React.Fragment>
-            {props.policylist.map((item) => (
-                <li className="underline lister" style={{}}key={item.id}><a href={`assets/policies/${item.name}`} download={`assets/policies/${item.name}`}>{item.name} <span className="fa fa-file-pdf-o" style={{color: '#f74040'}}></span></a></li>
-            ))}
-        </React.Fragment>
-    )
-}
-export default PolicyList
+  return (
+    <React.Fragment>
+      {props.policylist.map((item) => (
+        <li className="underline lister" style={{}} key={item.id}>
+          <a
+            href={`assets/policies/${item.name}`}
+            download={`assets/policies/${item.name}`}
+          >
+            {item.name}{" "}
+            <span
+              className="fa fa-file-pdf-o"
+              style={{ color: "#f74040" }}
+            ></span>
+          </a>
+        </li>
+      ))}
+    </React.Fragment>
+  );
+};
+export default PolicyList;
